0e7f334357b93fbe57d8db9e33cbc6d76aa0692f
perrettJ4 <perrettj4@gmail.com>
2022-01-13 15:29:12 +0000

    video player up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
-// import image1 from "../public/images/image1.png";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [isPlaying, setPlaying] = useState(false);
-  const handleEvent = (e) => {
+  const [imageState, setImageState] = useState([{visible: false, count: 0}, {visible: false, count: 0}, {visible: false, count: 0}]);
+
+  const handleTime = (e) => {
     setCurrentTime(e.target.currentTime);
   };
+  const isVisible = (id) => {
+    switch (id) {
+      case "image1":
+        const action = currentTime >= 3.5 && currentTime < 8.5;
+        action && 
+        return ;
+      case "image2":
+        return currentTime >= 6.0 && currentTime < 8.0;
+      case "image3":
+        return currentTime >= 7 && currentTime < 8.5;
+
+      default:
+      // code block
+    }
+    return;
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">Big Buck Bunny</header>
@@ -20,29 +39,33 @@ function App() {
             width="620"
             src="./Big_Buck_Bunny_1080_10s_5MB.mp4"
             type="video/webm"
-            onTimeUpdate={handleEvent}
+            onTimeUpdate={handleTime}
             onPlaying={() => setPlaying(true)}
             onPause={() => setPlaying(false)}
           >
             Sorry, your browser doesn't support embedded videos.
           </video>
+
           <img
             src={process.env.PUBLIC_URL + "/images/image1.png"}
             alt="banana"
             id="image1"
-            class="overlay"
+            className="overlay"
+            style={{ display: isVisible("image1") ? "block" : "none" }}
           />
           <img
             src={process.env.PUBLIC_URL + "/images/image2.png"}
             alt="banana"
             id="image2"
-            class="overlay"
+            className="overlay"
+            style={{ display: isVisible("image2") ? "block" : "none" }}
           />
           <img
             src={process.env.PUBLIC_URL + "/images/image3.png"}
             alt="banana"
             id="image3"
-            class="overlay"
+            className="overlay"
+            style={{ display: isVisible("image3") ? "block" : "none" }}
           />
         </div>
         <div style={{ color: "white" }}>elapsed time {currentTime}</div>
@@ -63,15 +86,3 @@ export default App;
 // img
 // create reusable component with start, end time props,
 // number of on/offs
-
-// const imageComp = (startTime, endTime, Xpos, Ypos, numOfDisplays) => {
-//   position === 'top'
-//   return (
-//     <img
-//       src={process.env.PUBLIC_URL + "/images/image1.png"}
-//       alt="banana"
-//       id="image1"
-//       class="overlay"
-//     />
-//   );
-// }
